3a6e25898bdbb52e8992f4989002f264ab747b70
jmtae <alsxo7596@ajou.ac.kr>
2024-06-16 16:46:49 +0900

    왜 안되는거징

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-print("안녕하세요")
-
 i = 0
-for i in range(1,5):
-    print(i)
+j =0
+for i in range(2, 10):  # 2단부터 9단까지
+    print(f"{i}단:")
+    for j in range(1, 10):  # 각 단의 1부터 9까지
+        print(f"{i} x {j} = {i * j}")
+    print()  # 각 단 사이에 빈 줄 추가
