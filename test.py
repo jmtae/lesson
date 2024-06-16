f2a0feeae0a91309e99ef9543586b6316680fc27
jmtae <alsxo7596@ajou.ac.kr>
2024-06-16 16:48:20 +0900

    파이썬파일

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-i = 0
-j =0
-for i in range(2, 10):  # 2단부터 9단까지
-    print(f"{i}단:")
-    for j in range(1, 10):  # 각 단의 1부터 9까지
-        print(f"{i} x {j} = {i * j}")
-    print()  # 각 단 사이에 빈 줄 추가
+print('Hello world')
