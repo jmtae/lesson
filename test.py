c0f912e201b3c99886c07a84c64d073b587e6afc
jmtae <alsxo7596@ajou.ac.kr>
2024-06-16 16:50:49 +0900

    이제는 되는건가?

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('Hello world')
+# 구구단 출력 코드
+
+for i in range(2, 10):  # 2단부터 9단까지
+    print(f"{i}단:")
+    for j in range(1, 10):  # 각 단의 1부터 9까지
+        print(f"{i} x {j} = {i * j}")
+    print()  # 각 단 사이에 빈 줄 추가
